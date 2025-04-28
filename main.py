2be136dab3d1bae993b6d3f2fa2c3ec4642a98a3
Đoàn <izzykalas5@gmail.com>
2025-04-28 21:09:58 +0700

    gdf

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-print('nhap a so nguen :')
-a=int(input())
-b=int(input())
-c=int(input())
+print('nhap a so thuc :')
+a=int(float())
+b=int(float())
+c=int(float())
 def tich(a,b,c):
-    print("Tích 3 số nguyên là: ",a*b*c)
+    print("Tích 3 số thực là: ",a*b*c)
 def hieu(a,b,c):
-    print("Hiệu 3 số nguyên là: ",a-b-c)
+    print("Hiệu 3 số thực là: ",a-b-c)
 def tong(a,b,c):
-    print("Tổng 3 số nguyên là: ",a+b+c)
+    print("Tổng 3 số thực là: ",a+b+c)
 tong(a,b,c)
 hieu(a,b,c)
 tich(a,b,c)
