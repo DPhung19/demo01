49a9360522c65ce1d09497283765d3ca53958983
Đoàn <izzykalas5@gmail.com>
2025-04-28 20:53:38 +0700

    ff

---
+++ main.py
@@ -0,0 +1,13 @@
+print('nhap a so thuc:')
+a=float(input())
+b=float(input())
+c=float(input())
+def tich(a,b,c):
+    print("Tích 3 số thực là: ",a*b*c)
+def hieu(a,b,c):
+    print("Hiệu 3 số thực là: ",a-b-c)
+def tong(a,b,c):
+    print("Tổng 3 số thực là: ",a+b+c)
+tong(a,b,c)
+hieu(a,b,c)
+tich(a,b,c)
